6a5b2730ad1f3229df0be468de0b1210d2c4db36
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2024-04-01 23:19:39 +0000

    Changed from branch yassin line 4

--- Etudiant.java
+++ Etudiant.java
@@ -3,6 +3,6 @@ class Etudiant{
     private double moyenne;
     private double note;
     private String xi3ar = "3axa lmalik";
-    private int notes;
+    public String lastname = "BenLaden";
 
 }
