db3e9a396f47445109bfad73bf63538b974bc726
Yassin-Zaher <yassinzaherelfeliri@gmail.com>
2024-04-01 23:44:55 +0000

    gga

--- Etudiant.java
+++ Etudiant.java
@@ -1,9 +1,10 @@
 class Etudiant{
-    private String nom;
+    private String nom = 'yassin';
     private double moyenne;
     private double note;
     private int number;
     private int id;
+
 
 
 }
